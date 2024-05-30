ab060a45e7558903d2353a95afa42159f460fad5
AndikaBN <bintangnursalih275@gmail.com>
2024-05-30 18:03:55 +0800

    change order(6)

--- database/migrations/2024_05_28_144338_create_order_table.php
+++ database/migrations/2024_05_28_144338_create_order_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('data_order', function (Blueprint $table) {
+        Schema::create('order', function (Blueprint $table) {
             $table->id();
             $table->string('kode_order');
             $table->foreignId('data_otlets_id')->constrained('data_otlets');
